ba66248d7fac7ec498ed9e5affe055a4b286e774
ilBarlo <f.barletta14ps4@gmail.com>
2020-11-26 20:58:43 +0100

    Barlo ha effetuato delle modifiche

--- APPLICATION/app/src/main/java/com/example/asuper/Beacon.java
+++ APPLICATION/app/src/main/java/com/example/asuper/Beacon.java
@@ -1,33 +1,49 @@
 package com.example.asuper;
 
 public class Beacon {
-
-    //dichiarazione stringhe
-    private String codice;
-    private String locazione;
-
-    //crea oggetto
-    public Beacon(String codice, String locazione) {
-        this.codice = codice;
-        this.locazione = locazione;
+    /*private BeaconManager mBeaconManager;
+    public void onResume() {
+        super.onResume();
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        //Riconosce il mainframe UID dell’EddystoneBeacon:
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+        mBeaconManager.bind(this);
     }
 
-    //get e set
-    public void setCodice(String codice){
-        this.codice = codice;
-    }
-    public String getCodice(){
-        return codice;
-    }
-    public void setLocazione(String locazione) {
-    this.locazione= locazione;
-    }
-    public String getLocazione() {
-    return locazione;
+    @Override
+    public void onBeaconServiceConnect() {
+        Identifier myBeaconNamespaceId = Identifier.parse("NameSpaceID del Beacon");
+        Identifier myBeaconInstanceId = Identifier.parse("IstanceID del beacon");
+        Region region = new Region("my-beacon-region", myBeaconNamespaceId, myBeaconInstanceId, null);
+        mBeaconManager.addMonitorNotifier(this);
+        try {
+            mBeaconManager.startMonitoringBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
-    public Beacon getBeacon (){
-        return this;
+    public void didEnterRegion (Region region) {
+        //incrementare contatore in supermercato
+        //1° BEACON VICINO L'ENTRATA
     }
+
+    public void didExitRegion (Region region) {
+        //decrementare contatore in supermercato
+        //2° BEACON VICINO L'USCITA
+    }
+
+    public void didDetermineStateForRegion (int state, Region region) {
+        //POSSIBILE IMPLEMENTAZIONE (?)
+        //Non utile ai fini della nostra applicazione
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mBeaconManager.unbind(this);
+    }
+
+     */
 }
-
